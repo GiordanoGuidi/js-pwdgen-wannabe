974d9464a4c076f02b0e5c0a7cd103b6d49e020a
GiordanoGuidi <giordanoguidi23@gmail.com>
2023-12-15 18:33:21 +0100

    added name and surname variables

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+console.log('JS OK')
+// Prendo l'elemento nell'html//
+const paragraph= document.getElementById('password');
+console.log(paragraph);
+//Chiedo all'utente il nome e creo una variabile//
+const firstname= prompt('Come ti chiami ', 'Giordano')
+//Chiedo all'utente il Cognome e creo una variabile//
+const surname= prompt("Qual'è il tuo cognome?", 'Guidi')
+
 
 
 
